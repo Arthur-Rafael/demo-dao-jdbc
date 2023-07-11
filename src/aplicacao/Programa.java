d5530f80799e3479bbc578c8b30ce13bdeca113a
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-11 13:09:49 -0300

    Classe entidade Vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,18 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entitidades.Departamento;
+import model.entitidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "Books");
-		System.out.println(obj);
+		
+		Vendedor vendedor = new Vendedor(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(vendedor);
 	}
-
 }
