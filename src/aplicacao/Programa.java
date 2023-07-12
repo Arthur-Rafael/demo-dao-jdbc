9ab4d8575cf597b9adcb6b9902981c8af8ecdb05
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-12 12:00:49 -0300

    Implementando o findByDepartment

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,7 +1,10 @@
 package aplicacao;
 
+import java.util.List;
+
 import model.dao.FabricaDeDao;
 import model.dao.VendedorDao;
+import model.entitidades.Departamento;
 import model.entitidades.Vendedor;
 
 public class Programa {
@@ -11,9 +14,16 @@ public class Programa {
 		VendedorDao vendedorDao = FabricaDeDao.criaVendedorDao();
 		
 		System.out.println("=== Teste Nº1: Vendedor findById ===");
-		
 		Vendedor vendedor = vendedorDao.findById(3);
-		
 		System.out.println(vendedor);
+		
+		System.out.println();
+		
+		System.out.println("=== Teste Nº2: Vendedor findByDepartment ===");
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> lista = vendedorDao.findByDepartment(departamento);
+		for(Vendedor obj : lista) {
+			System.out.println(obj);
+		}
 	}
 }
