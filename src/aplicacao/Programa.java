0472b64c11a34c495d953b6443f223096209c810
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-12 11:22:18 -0300

    Implementando o findById

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,21 +1,16 @@
 package aplicacao;
 
-import java.util.Date;
-
 import model.dao.FabricaDeDao;
 import model.dao.VendedorDao;
-import model.entitidades.Departamento;
 import model.entitidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Departamento obj = new Departamento(1, "Books");
-		
-		Vendedor vendedor = new Vendedor(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
 		VendedorDao vendedorDao = FabricaDeDao.criaVendedorDao();
+		
+		Vendedor vendedor = vendedorDao.findById(3);
 		
 		System.out.println(vendedor);
 	}
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDao;
+import model.entitidades.Departamento;
 import model.entitidades.Vendedor;
 
 public class VendedorDaoJDBC implements VendedorDao{
 
+	private Connection conn;
+	
+	public VendedorDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Vendedor obj) {
 		
@@ -27,8 +40,37 @@ public class VendedorDaoJDBC implements VendedorDao{
 
 	@Override
 	public Vendedor findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT seller.*, department.Name as DepName" +
+					" FROM seller INNER JOIN department" +
+					" ON seller.DepartmentId = department.Id" + 
+					" WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("DepartmentID"));
+				dep.setNome(rs.getString("DepName"));
+				Vendedor vendedor = new Vendedor();
+				vendedor.setId(rs.getInt("Id"));
+				vendedor.setNome(rs.getString("Name"));
+				vendedor.setEmail(rs.getString("Email"));
+				vendedor.setSalarioBase(rs.getDouble("BaseSalary"));
+				vendedor.setDataDeNascimento(rs.getDate("BirthDate"));
+				vendedor.setDepartamento(dep);
+				return vendedor;
+			}
+			return null;
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 		
-		return null;
 	}
 
 	@Override
