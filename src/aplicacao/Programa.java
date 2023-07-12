96add90b8f1b22ad641a0ec4396c2e105b823f47
Arthur Rafael <arthurrsj123@gmail.com>
2023-07-12 13:01:57 -0300

    Implementando o update

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -29,7 +29,7 @@ public class Programa {
 		
 		System.out.println();
 		
-		System.out.println("=== Teste Nº3: Vendedor findByAll ===");
+		System.out.println("=== Teste Nº3: Vendedor findAll ===");
 		lista = vendedorDao.findAll();
 		for(Vendedor obj : lista) {
 			System.out.println(obj);
@@ -37,9 +37,17 @@ public class Programa {
 		
 		System.out.println();
 		
-		System.out.println("=== Teste Nº4: Vendedor findByAll ===");
+		System.out.println("=== Teste Nº4: Vendedor Insert ===");
 		Vendedor novoVendedor = new Vendedor(null, "Greg", "greg@gmail.com", new Date(), 4000.0, departamento);
 		vendedorDao.insert(novoVendedor);
 		System.out.println("Inserido. Novo Id = " + novoVendedor.getId());
+		
+		System.out.println();
+		
+		System.out.println("=== Teste Nº5: Vendedor Update ===");
+		vendedor = vendedorDao.findById(1);
+		vendedor.setNome("Sarah Cameron");
+		vendedorDao.update(vendedor);
+		System.out.println("Atualização feita");
 	}
 }
